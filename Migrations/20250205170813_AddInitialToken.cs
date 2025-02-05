2b7d59b29a68884822cde7bea32036cae5709453
SvetaSliusar <svetaslusar2@gmail.com>
2025-02-05 17:12:09 +0000

    add migration with test token

---
+++ Migrations/20250205170813_AddInitialToken.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TelegramBookingBot.Migrations
+{
+    public partial class AddInitialToken : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                "INSERT INTO public.\"Tokens\" (\"Id\", \"TokenValue\", \"Type\", \"ChatId\", \"CreatedAt\", \"Used\", \"CompanyId\", \"ClientId\") " +
+                "VALUES (1, '123', 1, null, NOW(), false, null, null);"
+            );
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM public.\"Tokens\" WHERE \"TokenValue\" = 'initial-token-value';");
+        }
+    }
+}
